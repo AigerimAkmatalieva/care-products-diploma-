43898a0732be3da901c1f8ec9ed84096a7d16a29
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-11 15:27:29 +0600

    added file css

---
+++ src/components/Navbar/NavItem/NavItem.js
@@ -0,0 +1,12 @@
+import "./NavItem.css";
+
+function NavItem(props) {
+ 
+  return (
+    <li className="NavItem">
+      <a href={props.url}>{props.children}</a>
+    </li>
+  )
+}
+
+export default NavItem;
