72bf109a5dd31c262b21cdd2ef5f0badd6b4b122
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-13 14:28:33 +0600

    added components

---
+++ src/pages/Delivery.js
@@ -0,0 +1,11 @@
+function Contacts() {
+ 
+  return (
+   <article>
+      <h1>Care Cosmetics Delivery</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque incidunt odio rem modi laudantium. Tempora consequatur error natus, neque expedita maiores ut temporibus tempore sed obcaecati quidem nemo molestias nobis.</p>
+   </article>
+  )
+}
+
+export default Contacts;
