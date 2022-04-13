8ced8a59f1206dbca12e621ab73102188442dcf1
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-13 07:12:03 +0600

    new components NavItem

--- src/components/Navbar/NavItem/NavItem.js
+++ src/components/Navbar/NavItem/NavItem.js
@@ -1,5 +1,6 @@
-import { NavLink } from "react-router-dom";
 import "./NavItem.css";
+import { NavLink } from "react-router-dom";
+
 
 function NavItem(props) {
   return (
---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+function About() {
+ 
+  return (
+   <article>
+      <h1>Care Cosmetics</h1>
+    <p>loremd hello world</p>
+   </article>
+  )
+}
+
+export default About;
