8a765b50b453356664e533ec46a2501277dbff78
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-13 07:11:15 +0600

    import components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import Nav from "./components/Nav";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
+import About from "./pages/About";
+import Catalog from "./pages/Catalog";
+import Contacts from "./pages/Contacts";
+import Delivery from "./pages/Delivery";
+import Search from "./pages/Search";
+import Signup from "./pages/Signup";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
      <Nav />
      <Header />
+
+9
+     <Routes>
+       <Route path="/about" element={<About />}/>
+       <Route path="/catalog" element={<Catalog />}/>
+       <Route path="/contacts" element={<Contacts />}/>
+       <Route path="/delivery" element={<Delivery />}/>
+       <Route path="/search" element={<Search />}/>
+       <Route path="/signup" element={<Signup />}/>
+     </Routes>
      <Footer />
     </div>
   );
