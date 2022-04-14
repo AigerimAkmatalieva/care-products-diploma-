4848ba1871124139294cdf974be321455a298b36
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-14 07:07:50 +0600

    correct components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,6 @@ function Header() {
   return (
     <header className="Header" style={style}>
       <h1>Get Your Best Cosmetic Products Here</h1>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
     </header>
   );
 }
