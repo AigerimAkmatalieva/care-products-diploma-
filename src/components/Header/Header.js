15bc98cf3244e11e987856cc52ffa09e46933d96
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-14 20:53:48 +0600

    added new file carouselbox

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,10 @@
 import "./Header.css";
-import header from "../../assets/header.svg";
+import CarouselBox from "../../components/CarouselBox";
 
 function Header() {
-  const style = {
-    backgroundImage: `url(${header})`,
-  };
 
   return (
-    <header className="Header" style={style}>
-      <h1>Get Your Best Cosmetic Products Here</h1>
-      <p>Lorem Ipsum is simply dummy text of the <br/> printing and typesetting industry. <br/></p>
-    </header>
+    <CarouselBox />
   );
 }
 export default Header;
