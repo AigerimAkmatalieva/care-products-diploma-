013ed917bb060b02d7505fe2945211fcb3b6bcf4
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-14 06:00:31 +0600

    added nw images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,15 @@
 import "./Header.css";
-import header2Image from "../../assets/header2.svg";
+import header3Image from "../../assets/header2.svg";
 
 function Header() {
   const style = {
-    backgroundImage: `url(${header2Image})`,
+    backgroundImage: `url(${header3Image})`,
   };
 
   return (
     <header className="Header" style={style}>
-      <h1>Care products</h1>
-      <p>Shop best selling skin care products.</p>
+      <h1>Get Your Best Cosmetic Products Here</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
     </header>
   );
 }
