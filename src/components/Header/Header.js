4c4872bbe56f0c0cd2d23bfc8d4cb015ff62b9a9
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-14 07:23:53 +0600

    correct components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import "./Header.css";
-import header from "../../assets/header3.svg";
+import header from "../../assets/header2.svg";
 
 function Header() {
   const style = {
@@ -9,6 +9,7 @@ function Header() {
   return (
     <header className="Header" style={style}>
       <h1>Get Your Best Cosmetic Products Here</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
     </header>
   );
 }
