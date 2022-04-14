8c87db48d91ee73fa05d344b570f5a22ad476020
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-14 22:23:46 +0600

    addded components

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,14 @@
 import "../pages/pages.css";
+import React, { components } from "react";
+import CarouselBox from "../components/CarouselBox.js";
 
 
 function About() {
  
   return (
-   <article>
-      <h1>About Care Cosmetics</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque incidunt odio rem modi laudantium. Tempora consequatur error natus, neque expedita maiores ut temporibus tempore sed obcaecati quidem nemo molestias nobis.</p>
-   </article>
+   <div>
+     <CarouselBox />
+   </div>
   )
 }
 
