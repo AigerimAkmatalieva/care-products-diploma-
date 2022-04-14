b3157c1d58b6b150d17a7d5f8e7690e318d0ed3b
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-14 06:42:50 +0600

    added components

---
+++ src/pages/Orders.js
@@ -0,0 +1,11 @@
+function Orders() {
+ 
+  return (
+   <article>
+      <h1>Orders</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque incidunt odio rem modi laudantium. Tempora consequatur error natus, neque expedita maiores ut temporibus tempore sed obcaecati quidem nemo molestias nobis.</p>
+   </article>
+  )
+}
+
+export default Orders;
