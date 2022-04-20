5e1ac72984a144f480877db47e4af71f7e322658
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-20 15:34:57 +0600

    Renew styles

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -34,7 +34,7 @@ function Catalog() {
   return (
     <>
       <Header
-        title="Pick your type of coffee!"
+        title="Catalog"
         image={productImage}>
         The fruits; cherries or berries, most commonly contain two stones with their flat sides together. A small percentage of cherries contain a single seed, instead of the usual two. This is called a "peaberry".
       </Header>
