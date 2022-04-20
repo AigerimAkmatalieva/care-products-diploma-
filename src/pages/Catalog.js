472ecb82b26d4287fa8a2b8641fa9e7a7d18c086
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-20 15:12:13 +0600

    Renew products pages

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,15 +1,45 @@
 import Header from "../components/Header/Header";
-import headerImage from "../assets/header3.svg";
+import ProductList from "../components/ProductList/ProductList";
+
+import productImage from "../assets/header3.svg";
 
 function Catalog() {
+  const products = {
+    hazelnut: {
+      image: productImage,
+      path: "/",
+      title: "Hazelnut",
+      price: 10,
+    },
+    darkRoast: {
+      image: productImage,
+      path: "/",
+      title: "Dark roast",
+      price: 12,
+    },
+    houseBlend: {
+      image: productImage,
+      path: "/",
+      title: "House blend",
+      price: 14,
+    },
+    lightRoast: {
+      image: productImage,
+      path: "/",
+      title: "Light roast",
+      price: 13,
+    }
+  };
+
   return (
     <>
-      <Header title="Catalog" image={headerImage}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque incidunt
-        odio rem modi laudantium. Tempora consequatur error natus, neque
-        expedita maiores ut temporibus tempore sed obcaecati quidem nemo
-        molestias.
+      <Header
+        title="Pick your type of coffee!"
+        image={headerImage}>
+        The fruits; cherries or berries, most commonly contain two stones with their flat sides together. A small percentage of cherries contain a single seed, instead of the usual two. This is called a "peaberry".
       </Header>
+
+      <ProductList products={products} />
     </>
   );
 }
