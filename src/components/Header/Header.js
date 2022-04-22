12cfb577cb2d073158c362ceeb270dcddb787259
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-22 14:38:26 +0600

    Renew header components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,13 @@ import "./Header.css";
 
 function Header({ image, title, children }) {
   return (
-    <header className="Header">
-      <img src={image} />
-      <section>
-        <h1>{title}</h1>
-        <p>{children}</p>
-      </section>
-    </header>
+      <header className="Header">
+        <img src={image} alt="header"/>
+        <section>
+          <h1>{title}</h1>
+          <p>{children}</p>
+        </section>
+      </header>
   );
 }
-
 export default Header;
