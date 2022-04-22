e12d2c7267c8029a1821068d1fce8b519b06de00
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-22 14:33:39 +0600

    Renew components

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/delivery" element={<Delivery />} />
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import Nav from "../Navbar/Nav";
 import Footer from "../Footer/Footer";
+import Home from "../../pages/Home";
 
 function Layout({ children }) {
   return (
     <main className="Layout">
       <Nav />
+      <Home />
       <div className="container">{children}</div>
       <Footer />
     </main>
