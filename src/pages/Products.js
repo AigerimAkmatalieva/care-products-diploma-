12e706de7257d10f3c09231a6c980d8d732699a2
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-25 23:14:09 +0600

    renew components

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,12 @@
 import Header from "../components/Header/Header";
 import ProductList from "../components/ProductList/ProductList";
 import { getProduct } from "../data/products";
-import productImage from "../assets/header2.svg";
+import headerImage from "../assets/header2.svg";
 
 function Products() {
+  const styles = {
+    backgroundImage: `url(${headerImage})`
+  }
   return (
      <>
      <Header className="Header" style={styles}>
