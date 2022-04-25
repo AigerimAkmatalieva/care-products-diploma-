58008948042a97ad20519b8e4d3575d297b518e4
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-04-25 06:50:30 +0600

    Added components

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -2,10 +2,10 @@ import ProductItem from "./ProductItem/ProductItem";
 import classes from "./ProductList.module.css";
 
 function ProductList({ products }) {
-  const productItems = products.map((product) => (
+  const productItem = products.map((product) => (
     <ProductItem key={product.productId} product={product} />
   ));
 
-  return <div className={classes.ProductList}>{productItems}</div>;
+  return <div className={classes.ProductList}>{productItem}</div>;
 }
 export default ProductList;
