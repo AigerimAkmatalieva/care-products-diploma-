2f28bbe8591027f4810de52226c1c39094602d70
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-05-12 22:09:17 +0600

    Renew components

--- src/pages/Home/Menu/Menu.js
+++ src/pages/Home/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./Menu.module.css";
+import cardImages from "../../assets/card.svg";
 
 function Menu() {
   return (
@@ -8,24 +9,28 @@ function Menu() {
         <div className="row">
           <div className="col-3">
           <div className={classes.card}>
+          <img src={cardImages} alt="Card"/>
             <h2>Master Chefs</h2>
             <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita minima tempore eius voluptatum animi ullam natus facilis</p>
           </div>
           </div>
           <div className="col-3">
           <div className={classes.card}>
+          <img src={cardImages} alt="Card"/>
           <h2>Quality Food</h2>
           <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita minima tempore eius voluptatum animi ullam natus facilis</p>
           </div>
           </div>
           <div className="col-3">
           <div className={classes.card}>
+          <img src={cardImages} alt="Card"/>
           <h2>Online Order</h2>
           <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita minima tempore eius voluptatum animi ullam natus facilis</p>
           </div>
           </div>
           <div className="col-3">
           <div className={classes.card}>
+          <img src={cardImages} alt="Card"/>
           <h2>24/7 Services</h2>
           <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita minima tempore eius voluptatum animi ullam natus facilis</p>
           </div>
