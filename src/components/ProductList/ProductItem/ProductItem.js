fbf6d1125697021e7553f59bd4944c4a879a6dc0
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-05-18 14:52:04 +0600

    Renew components

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -6,6 +6,7 @@ function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
       <img src={product.image} alt="product.title" />
+      <br/>
       <Link to={"/products/" + product.productId}>{product.title}</Link>
       <div className={classes.price}>{product.price}$</div>
     </div>
