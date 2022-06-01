a784ed6942152f434fb893da2ed79e6df8bf7398
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-06-01 14:28:38 +0600

    Renew components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,12 @@ function Header({ image, title, p, children }) {
     <header className="Header">
       <div className="container">
       <div className="row">
-        <div className="col-7">
+        <div className="col-6">
           <h1>{title}</h1>
           <p>{children}</p>
           <button>{p}</button>
         </div>
-        <div className="col-5">
+        <div className="col-6">
           <img src={image} alt="img" />
         </div>
       </div>
