2e76ea40cf15b6e2b155b8b2a477ea87dc736df4
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-06-29 14:13:19 +0600

    Add CartDisplay and CartItem components

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -7,24 +7,44 @@ export default function CartItem({ product, actions, quantity }) {
   const dispatch = useDispatch();
 
   return (
-    <div className={classes.CartItem}>
-      <div className={classes.image}>
-        <img src={product.image} alt={product.title} />
+    <>
+      <div className={classes.CartItem}>
+        <div className={classes.CartItem1}>
+          <img
+            src={product.image}
+            alt={product.title}
+            className={classes.image}
+          />
+         <span className={classes.span}>
+         <div className={classes.hero}>
+            <Link to="" className={classes.title}>
+              {product.title}{" "}
+            </Link>
+          </div>
+          <div className={classes.quantity}>
+            {actions ? (
+              <button onClick={() => dispatch(decrement(product.productId))}>
+                -
+              </button>
+            ) : null}
+            {quantity}
+            {actions ? (
+              <button onClick={() => dispatch(increment(product.productId))}>
+                +
+              </button>
+            ) : null}
+          </div>
+          <div className={classes.subtotal}>
+            ${product.price * quantity}
+            {actions ? (
+              <button onClick={() => dispatch(remove(product.productId))}>
+                Delete
+              </button>
+            ) : null}
+          </div>
+         </span>
+        </div>
       </div>
-     <div className={classes.CartItem2}>
-     <div className={classes.hero}>
-        <Link to="" className={classes.title}>{product.title} </Link>
-      </div>
-      <div className={classes.quantity}>
-        {actions ? <button onClick={() => dispatch(decrement(product.productId))}>-</button> : null}
-        {quantity}
-        {actions ? <button onClick={() => dispatch(increment(product.productId))}>+</button> : null}
-      </div>
-      <div className={classes.subtotal}>
-        ${product.price * quantity}
-        {actions ? <button onClick={() => dispatch(remove(product.productId))}>Delete</button> : null}
-      </div>
-     </div>
-    </div>
-  )
+    </>
+  );
 }
